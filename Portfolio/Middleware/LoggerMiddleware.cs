43e396779273418b4fa2fe3251be4bf1bf2d2462
DanilMir <mirgayazov02@gmail.com>
2022-02-19 12:22:22 +0300

    logger

---
+++ Portfolio/Middleware/LoggerMiddleware.cs
@@ -0,0 +1,17 @@
+﻿namespace Portfolio.Middleware;
+
+public class LoggerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public LoggerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        Console.WriteLine(context.Connection);
+        await _next.Invoke(context);
+    }
+}
