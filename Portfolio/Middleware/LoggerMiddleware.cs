91d81fbef9838af62ca28ac15a51c420cb855b40
DanilMir <mirgayazov02@gmail.com>
2022-02-20 21:12:10 +0300

    logger interpolation string

--- Portfolio/Middleware/LoggerMiddleware.cs
+++ Portfolio/Middleware/LoggerMiddleware.cs
@@ -14,9 +14,12 @@ public class LoggerMiddleware
         var queryStr = context.Request.Query.Aggregate("", (current, q) => 
             $"{current}{q.Key}={q.Value}\n");
 
-        Console.WriteLine(
-            $"Request:\nTime: {DateTime.Now}\nClient IP address: {context.Connection.RemoteIpAddress}\n{context.Request.Protocol} {context.Request.Method}\nPATH: {context.Request.Scheme}://{context.Request.Host}{context.Request.Path}\nParams: {queryStr}"
-        );
+        Console.WriteLine($@"Request:
+Time: {DateTime.Now}
+Client IP address: {context.Connection.RemoteIpAddress}
+{context.Request.Protocol} {context.Request.Method}
+PATH: {context.Request.Scheme}://{context.Request.Host}{context.Request.Path}
+Params: {queryStr}");
         await _next.Invoke(context);
     }
 }
